d9eef023ad73733031107cf045cf830e42fb78bb
Moshe Sivan <msivan@Moshes-MacBook-Pro.local>
2017-09-09 22:13:21 +0300

    Node engine

--- deploy.cmd
+++ deploy.cmd
@@ -1,6 +1,4 @@
-set NODE_EXE=D:\Program Files (x86)\node\6.2.2\node.exe
-set NPM_CMD=D:\Program Files (x86)\npm\3.9.5\npm.cmd
+set PATH=D:\Program Files (x86)\node\6.2.2;D:\Program Files (x86)\npm\3.9.5;%PATH%
 
 "%NPM_CMD%" install
-grunt 
-"%NPM_CMD%" start
+grunt
