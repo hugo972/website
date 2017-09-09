d37d257cc9d092feaa764dd86236468667e9391f
Moshe Sivan <msivan@Moshes-MacBook-Pro.local>
2017-09-09 23:27:05 +0300

    Node engine

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,5 @@
-set PATH=D:\Program Files (x86)\node\6.2.2;D:\Program Files (x86)\npm\3.9.5;%PATH%
+setlocal
+set PATH=D:\Program Files (x86)\nodejs\6.2.2;D:\Program Files (x86)\npm\3.9.5;%PATH%
 
 call npm install
 call grunt
