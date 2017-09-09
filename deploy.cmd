c0780743b1075b4fe5d919deb4d190f8a92f235a
Moshe Sivan <msivan@Moshes-MacBook-Pro.local>
2017-09-09 22:36:26 +0300

    Node engine

--- deploy.cmd
+++ deploy.cmd
@@ -1,5 +1,7 @@
-setlocal enabledelayedexpansion
+setlocal
 set PATH=D:\Program Files (x86)\node\6.2.2;D:\Program Files (x86)\npm\3.9.5;%PATH%
+
 call npm install
 call grunt
+call robocopy node_modules\ ..\wwwroot\ /E
 call robocopy bin\ ..\wwwroot\ /E
