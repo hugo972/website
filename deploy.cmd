aad47c3fdc34d6e59cf89aae4746ce7d312b2290
msivan <msivan@microsoft.com>
2017-09-10 20:14:13 +0300

    node version

--- deploy.cmd
+++ deploy.cmd
@@ -1,25 +1,5 @@
 setlocal
-set PATH=%ProgramFiles(x86)%\nodejs\6.2.2;%ProgramFiles(x86)%\npm\3.9.5;%PATH%
+node -v
 
-echo --------------------------------------
-echo installing build dependencies
-echo --------------------------------------
-echo.
-call npm install
-echo --------------------------------------
-echo building
-echo --------------------------------------
-echo.
-call grunt
-echo --------------------------------------
-echo copying to deployment target
-echo --------------------------------------
-echo.
-call robocopy bin\ ..\wwwroot\ /E /NFL /NJH /NJS /nc /ns /np
-echo --------------------------------------
-echo installing server dependencies
-echo --------------------------------------
-echo.
-call npm install express --prefix ../wwwroot/
 
 endlocal
