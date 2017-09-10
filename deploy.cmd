30fa76c207db33181057d9ba9560f3cfef1a12b6
msivan <msivan@microsoft.com>
2017-09-10 18:35:22 +0300

    iisnode config

--- deploy.cmd
+++ deploy.cmd
@@ -4,5 +4,6 @@ set PATH=D:\Program Files (x86)\nodejs\6.2.2;D:\Program Files (x86)\npm\3.9.5;%P
 call npm install
 call grunt
 call robocopy bin\ ..\wwwroot\ /E /NP
+call robocopy node_modules\ ..\wwwroot\ /E /NP
 
 endlocal
